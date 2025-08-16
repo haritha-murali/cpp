aa5298a41b76a328b9d8787a0a5832b87ded6eeb
haritha-murali <88222222+haritha-murali@users.noreply.github.com>
2025-08-16 11:52:54 +0530

    Add files via upload

---
+++ basics/1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    unsigned char a = 123;
+    char b = 'A';
+    a++;
+    b++;
+    printf("%c\n", b);
+    printf("%d", a);
+
+    return 0;
+}
